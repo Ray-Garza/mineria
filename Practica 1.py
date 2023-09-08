abca25f68ff18070828da1a6b47f2ea7ed081092
Ray-Garza <98302213+Ray-Garza@users.noreply.github.com>
2023-09-07 21:11:28 -0600

    Practica 1, descargar el dataset

---
+++ Practica 1.py
@@ -0,0 +1,3 @@
+import opendatasets as od
+#Poner usuario de kaggle y llave de api para descargar el dataset 
+od.download("https://www.kaggle.com/datasets/guillemservera/precious-metals-data?resource=download")
